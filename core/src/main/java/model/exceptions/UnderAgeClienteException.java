314fe55adb2b78e09bdf05c4f955a29c8a023c14
habibu <habib.az@hotmail.com>
2017-11-16 10:46:37 +0100

    @n2017.11.16 Debugging Exceptions
    #3 Issue 1.0
    Implemented client age validation methods and exceptions using new branch created

--- core/src/main/java/model/exceptions/UnderAgeClienteException.java
+++ core/src/main/java/model/exceptions/UnderAgeClienteException.java
@@ -3,20 +3,9 @@ package model.exceptions;
 public class UnderAgeClienteException extends Exception
     {
 
-    public UnderAgeClienteException() {
-    }
 
     public UnderAgeClienteException(String s) {
         super(s);
     }
-
-    public UnderAgeClienteException(String s, Throwable throwable) {
-        super(s, throwable);
     }
 
-    public UnderAgeClienteException(Throwable throwable) {
-        super(throwable);
-    }
-
-    
-    }
